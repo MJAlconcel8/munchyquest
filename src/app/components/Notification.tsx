99ced5a9339c4a4c79f19ea1bf9828e368b66d1b
Mark Joseph Alconcel <109438699+MJAlconcel8@users.noreply.github.com>
2024-12-30 20:18:12 -0500

    setting up some pages and routing

---
+++ src/app/components/Notification.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Notification = () => {
+  return (
+    <div className=''>Notification</div>
+  )
+}
+
+export default Notification
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Notification from '../app/components/Notification';
+import Footer from "../app/components/Footer";
+import Navbar from "../app/components/Navbar";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +30,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Notification/>
+        <Navbar/>
         {children}
+        <Footer />
       </body>
     </html>
   );
